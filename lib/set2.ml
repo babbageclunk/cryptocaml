4dca90e0d9651d619d0c7ae327e20c09dc78cb1d
Christian Muirhead <xtian@babbageclunk.com>
2024-08-28 00:29:09 +1200

    Start of set2c3 - implemented the oracle I think

--- lib/set2.ml
+++ lib/set2.ml
@@ -29,3 +29,36 @@ let set2c2 () =
   Common.input ()
   |> aes_cbc_decrypt key (Bytes.make 16 '\000')
   |> Bytes.to_string
+
+
+let aes_cbc_encrypt key iv text =
+  let size = Bytes.length key in
+  let padded = pkcs7 size text in
+  let blocks = Common.blocks size padded in
+  let rec chain last blocks acc =
+    match blocks with
+    | [] -> List.rev acc |> Bytes.concat Bytes.empty
+    | b::bs ->
+      let this = Common.xor_bytes last b |> Common.aes_ecb key in
+      chain this bs (this::acc)
+  in
+  chain iv blocks []
+
+let randchar _ =
+  Random.int 256 |> Char.chr
+
+let randpadding () =
+  let size = Random.int_in_range ~min:5 ~max:10 in
+  Bytes.init size randchar
+
+let encryption_oracle text =
+  let rand_key = Bytes.init 16 randchar in
+  let rand_iv = Bytes.init 16 randchar in
+  let padded =
+    Bytes.concat Bytes.empty [randpadding (); text; randpadding ()]
+    |> pkcs7 16
+  in
+  if Random.bool () then
+    Common.aes_ecb rand_key padded
+  else
+    aes_cbc_encrypt rand_key rand_iv padded
